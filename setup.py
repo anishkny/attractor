68af63b73bab27db97a48842013392ef928e608e
Anish Karandikar <anishkny@gmail.com>
2026-02-07 18:05:18 -0800

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-attractor",
-    version="0.1.0",
+    version="0.1.1",
     description="A DOT-based pipeline runner for AI workflows",
     author="Attractor Contributors",
     packages=find_packages(where="src"),
