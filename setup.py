a2b1de07efe9d309c825c2c14264b2f1ad724965
Anish Karandikar <anishkny@gmail.com>
2026-02-07 17:58:47 -0800

    Rename package from "attractor" to "py-attractor" in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="attractor",
+    name="py-attractor",
     version="0.1.0",
     description="A DOT-based pipeline runner for AI workflows",
     author="Attractor Contributors",
